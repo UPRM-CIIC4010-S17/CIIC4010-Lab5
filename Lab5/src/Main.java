1526045679c51a442a2f1639949e039aeb67ec3c
jcvvdf <jcvvdf@gmail.com>
2017-03-28 23:34:50 -0400

    adding project solution to original fork

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -2,17 +2,21 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Color Grid");
+
+		JFrame myFrame = new JFrame("Minesweeper");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(325, 345);
 
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
-
-		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
-		myFrame.addMouseListener(myMouseAdapter);
-
 		myFrame.setVisible(true);
+		
+		BombSetter bombSetter = new BombSetter();
+		String[][] bombGrid = bombSetter.setBombs();
+		int emptySquares = 81 - bombSetter.getNumberOfBombs();
+		
+		MyMouseAdapter myMouseAdapter = new MyMouseAdapter(bombGrid, emptySquares);
+		myFrame.addMouseListener(myMouseAdapter);
 	}
 }
--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -1,17 +1,27 @@
 import java.awt.Color;
+
 import java.awt.Component;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Random generator = new Random();
+
+	private String[][] bombGrid;
+	public int emptySquares;
+
+	public MyMouseAdapter(String[][] bombGrid, int emptySquares) {
+		this.bombGrid = bombGrid;
+		this.emptySquares = emptySquares;
+	}
+
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
-		case 1:		//Left mouse button
+		case 1:
+		case 3:
 			Component c = e.getComponent();
 			while (!(c instanceof JFrame)) {
 				c = c.getParent();
@@ -33,36 +43,121 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
 			myPanel.repaint();
 			break;
-		case 3:		//Right mouse button
-			//Do nothing
-			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
 	}
+
+
+	public void revealSquare(MyPanel myPanel, int x, int y, String[][] bombGrid) {
+		if (myPanel.colorArray[x][y].equals(Color.white)) {
+			Color newColor = null;
+
+			switch (bombGrid[x][y]) {
+			case "0":
+				newColor = new Color(0xeeeeee);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				for (int d = -1; d <= 1; d++) {
+					if ((x + d >= 0) && (x + d < 9) && (y - 1 >= 0) && (y - 1 < 9)) { //index in range
+						revealSquare(myPanel, x + d, y - 1, bombGrid);
+					}
+				}
+
+				for (int d = -1; d <= 1; d += 2) {
+					if ((x + d >= 0) && (x + d < 9) && (y >= 0) && (y < 9)) { //index in range
+						revealSquare(myPanel, x + d, y, bombGrid);
+					}
+				}
+
+				for (int d = -1; d <= 1; d++) {
+					if ((x + d >= 0) && (x + d < 9) && (y + 1 >= 0) && (y+1 < 9)) { //index in range
+						revealSquare(myPanel, x + d, y + 1, bombGrid);
+					}
+				}
+				break;
+			case "1":
+				newColor = new Color(0xcccccc);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "2":
+				newColor = new Color(0xaaaaaa);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "3":
+				newColor = new Color(0x888888);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "4":
+				newColor = new Color(0x666666);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "5": 
+				newColor = new Color(0x444444);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "6":
+				newColor = new Color(0x333333);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "7":
+				newColor = new Color(0x222222);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "8":
+				newColor = new Color(0x111111);
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				emptySquares--;
+				break;
+			case "b":
+				newColor = Color.black;
+				myPanel.colorArray[x][y] = newColor;
+				myPanel.repaint();
+				break;
+			}
+
+		}
+	}
+
 	public void mouseReleased(MouseEvent e) {
+		Component c = e.getComponent();
+		while (!(c instanceof JFrame)) {
+			c = c.getParent();
+			if (c == null) {
+				return;
+			}
+		}
+		JFrame myFrame = (JFrame)c;
+		MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+		Insets myInsets = myFrame.getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		e.translatePoint(-x1, -y1);
+		int x = e.getX();
+		int y = e.getY();
+		myPanel.x = x;
+		myPanel.y = y;
+		int gridX = myPanel.getGridX(x, y);
+		int gridY = myPanel.getGridY(x, y);
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
-			Component c = e.getComponent();
-			while (!(c instanceof JFrame)) {
-				c = c.getParent();
-				if (c == null) {
-					return;
-				}
-			}
-			JFrame myFrame = (JFrame)c;
-			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
-			Insets myInsets = myFrame.getInsets();
-			int x1 = myInsets.left;
-			int y1 = myInsets.top;
-			e.translatePoint(-x1, -y1);
-			int x = e.getX();
-			int y = e.getY();
-			myPanel.x = x;
-			myPanel.y = y;
-			int gridX = myPanel.getGridX(x, y);
-			int gridY = myPanel.getGridY(x, y);
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
@@ -76,30 +171,21 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Do nothing
 					} else {
 						//Released the mouse button on the same cell where it was pressed
-						if ((gridX == 0) || (gridY == 0)) {
-							//On the left column and on the top row... do nothing
-						} else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
-							switch (generator.nextInt(5)) {
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
+
+						//On the grid other than on the left column and on the top row:
+
+						revealSquare(myPanel, myPanel.mouseDownGridX, myPanel.mouseDownGridY, bombGrid);
+
+						if (bombGrid[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == "b") {
+							emptySquares = -1;
+							for (int i = 0; i < 9; i++) {
+								for (int j = 0; j < 9; j++) {
+									if (myPanel.colorArray[j][i].equals(Color.white)) {
+										revealSquare(myPanel, j, i, bombGrid);
+									}
+								}
 							}
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-							myPanel.repaint();
+							JOptionPane.showMessageDialog(null, "Game Over");
 						}
 					}
 				}
@@ -107,11 +193,30 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			//Do nothing
-			break;
+			Color newColor = null;
+			if (myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY].equals(Color.white)) {
+				newColor = Color.red;
+			} else if (myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY].equals(Color.red)) {
+				newColor = Color.white;
+			} else {
+				break;
+			}
+			myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+			myPanel.repaint();
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
+		if (emptySquares == 0) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (myPanel.colorArray[j][i].equals(Color.white)) {
+						revealSquare(myPanel, j, i, bombGrid);
+					}
+				}
+			}
+			JOptionPane.showMessageDialog(null, "Congratulations! You Win!");
+		}
 	}
 }
+
